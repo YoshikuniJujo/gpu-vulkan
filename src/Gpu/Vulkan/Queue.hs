2d05fb7ceaed918b865897b9be270f2578cd8c24
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2024-12-05 14:15:22 +0900

    Gpu.Vulkan.Queue: bindSparse

--- src/Gpu/Vulkan/Queue.hs
+++ src/Gpu/Vulkan/Queue.hs
@@ -1,5 +1,9 @@
 {-# LANGUAGE ImportQualifiedPost #-}
-{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleContexts, FlexibleInstances #-}
+{-# LANGUAGE PatternSynonyms, ViewPatterns #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Gpu.Vulkan.Queue (
@@ -14,7 +18,7 @@ module Gpu.Vulkan.Queue (
 
 	-- * SPARSE RESOURCES
 
-	BindSparseInfo(..),
+	bindSparse, BindSparseInfo(..), BindSparseInfosToMiddle,
 
 	-- * ENUM
 
@@ -23,6 +27,7 @@ module Gpu.Vulkan.Queue (
 	) where
 
 import Data.TypeLevel.Tuple.Uncurry
+import Data.TypeLevel.Tuple.MapIndex
 import Data.HeteroParList qualified as HeteroParList
 import Data.Word
 
@@ -34,8 +39,10 @@ import Gpu.Vulkan.Queue.Middle qualified as M
 import Gpu.Vulkan.Queue.Enum
 
 import Data.TypeLevel.Maybe qualified as TMaybe
+import Data.HeteroParList (pattern (:**))
 import Data.HeteroParList qualified as HPList
 
+import Foreign.Storable.PeekPoke
 import Gpu.Vulkan.Device qualified as Device
 import Gpu.Vulkan.Semaphore.Internal qualified as Semaphore
 import Gpu.Vulkan.Sparse.Buffer.Internal qualified as Sparse.Buffer
@@ -47,6 +54,18 @@ submit q sis mf =
 	M.submit q (submitInfoListToMiddle sis) $ (\(Fence.F f) -> f) <$> mf
 
 type Index = Word32
+
+bindSparse :: (
+	BindSparseInfosToMiddle ias,
+	HPList.ToListWithCCpsM' WithPoked TMaybe.M (M0_6 ias) ) =>
+	Device.D sd -> M.Q ->
+	HPList.PL (U6 BindSparseInfo) ias -> Maybe (Fence.F sf) -> IO ()
+bindSparse dv q is mf = do
+	let	mmf = case mf of
+			Just (Fence.F f) -> Just f
+			Nothing -> Nothing
+	mis <- bindSparseInfosToMiddle dv is
+	M.bindSparse q mis mmf
 
 data BindSparseInfo mn swss bbs iobs ibs ssss = BindSparseInfo {
 	bindSparseInfoNext :: TMaybe.M mn,
@@ -85,3 +104,24 @@ bindSparseInfoToMiddle dv BindSparseInfo {
 		M.bindSparseInfoImageOpaqueBinds = miobs,
 		M.bindSparseInfoImageBinds = mibs,
 		M.bindSparseInfoSignalSemaphores = sss }
+
+class BindSparseInfosToMiddle bsias where
+	bindSparseInfosToMiddle :: Device.D sd ->
+		HPList.PL (U6 BindSparseInfo) bsias ->
+		IO (HPList.PL M.BindSparseInfo (M0_6 bsias))
+
+instance BindSparseInfosToMiddle '[] where
+	bindSparseInfosToMiddle _ HPList.Nil = pure HPList.Nil
+
+instance (
+	Sparse.Buffer.MemoryBindInfosToMiddle bbs,
+	Sparse.Image.OpaqueMemoryBindInfosToMiddle iobs,
+	Sparse.Image.MemoryBindInfosToMiddle ibs,
+	BindSparseInfosToMiddle bsias ) =>
+	BindSparseInfosToMiddle
+		('(mn, swss, bbs, iobs, ibs, ssss) ': bsias) where
+	bindSparseInfosToMiddle dv (U6 i :** is) = (:**)
+		<$> bindSparseInfoToMiddle dv i
+		<*> bindSparseInfosToMiddle dv is
+
+-- instance BindSparse
