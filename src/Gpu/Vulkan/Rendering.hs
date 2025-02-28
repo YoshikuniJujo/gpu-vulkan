c319cf46a3bc0bfeab250d1a8eab56e7b0a61657
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-02-28 16:20:56 +0900

    Gpu.Vulkan.Rendering: tribial

--- src/Gpu/Vulkan/Rendering.hs
+++ src/Gpu/Vulkan/Rendering.hs
@@ -1,6 +1,8 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Gpu.Vulkan.Rendering (
-	Info(..), AttachmentInfo(..) ) where
+	Info(..), AttachmentInfo(..),
+	AttachmentInfoMaybeToMiddle, AttachmentInfoListToMiddle, MaybeI0'7_8
+	) where
 
 import Gpu.Vulkan.Rendering.Internal
