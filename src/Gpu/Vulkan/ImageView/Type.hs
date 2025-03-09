7ed7d42a247a4af127a99993670bab191542bf77
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-03-10 08:11:40 +0900

    Gpu.Vulkan.ImageView.Type: instance ShowIO (I nm fmt si)

--- src/Gpu/Vulkan/ImageView/Type.hs
+++ src/Gpu/Vulkan/ImageView/Type.hs
@@ -1,12 +1,19 @@
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Gpu.Vulkan.ImageView.Type where
+module Gpu.Vulkan.ImageView.Type (I(..)) where
 
 import GHC.TypeLits
+import Text.Show.ToolsYj
 
 import Gpu.Vulkan.TypeEnum as T
 import Gpu.Vulkan.ImageView.Middle qualified as M
 
 newtype I (nm :: Symbol) (fmt :: T.Format) si = I M.I deriving Show
+
+instance ShowIO (I nm fmt si) where
+	showIO (I i) = do
+		si <- showIO i
+		pure $ "(I " ++ si ++ ")"
