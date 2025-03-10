09a6dd8361adaea5f53e929c354ecffa670004b2
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-03-10 16:12:11 +0900

    Gpu.Vulkan.Pipeline.Graphics.Type: instance ShowIO (G s vibs vias lyta)

--- src/Gpu/Vulkan/Pipeline/Graphics/Type.hs
+++ src/Gpu/Vulkan/Pipeline/Graphics/Type.hs
@@ -9,6 +9,7 @@ module Gpu.Vulkan.Pipeline.Graphics.Type (G(..)) where
 
 import GHC.TypeNats
 import Data.Kind
+import Text.Show.ToolsYj
 
 import Gpu.Vulkan.DescriptorSetLayout.Type qualified as DscStLyt
 import Gpu.Vulkan.Pipeline.Graphics.Middle qualified as M
@@ -17,3 +18,8 @@ import Gpu.Vulkan.VertexInput qualified as VertexInput
 
 newtype G s (vibs :: [(Type, VertexInput.Rate)]) (vias :: [(Nat, Type)])
 	(lyta :: (Type, [(Type, [DscStLyt.BindingType])], [Type])) = G M.G
+
+instance ShowIO (G s vibs vias lyta) where
+	showIO (G g) = do
+		sg <- showIO g
+		pure $ "(G " ++ sg ++ ")"
